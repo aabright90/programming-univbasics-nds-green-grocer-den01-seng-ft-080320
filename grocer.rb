07d81ecd7ed4af0d902cd1065f4cd70195bd682f
Austen Bright <pureaustenbright@gmail.com>
2020-03-27 00:28:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,16 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  hash_index = 0
+  while hash_index < collection.length do
+    if name == collection[hash_index][:item]
+    return collection[hash_index]
+    end
+    hash_index += 1
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,12 +18,55 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  arr = []
+  i = 0
+  while i < cart.length do
+    new_item = find_item_by_name_in_collection(cart[i][:item], arr)
+    if new_item != nil
+      new_item[:count] += 1
+    else
+      new_item = {
+        :item => cart[i][:item],
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        :count => 1
+      }
+      arr << new_item
+    end
+    i += 1
+  end
+  arr
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.length do
+    coupon = coupons[i][:item]
+    cart_item = find_item_by_name_in_collection(coupon, cart)
+    couponed_item_name = "#{coupon} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[i][:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[i][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[i][:cost] / coupons[i][:num],
+          :clearance => cart_item[:clearance],
+          :count => cart_item[:count]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[i][:num]
+      end
+    end
+    i += 1
+  end
+  cart
+  binding.pry
 end
 
 def apply_clearance(cart)
