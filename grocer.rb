57621cc0eab4004f3b9fb2b701ca5eac3cd39ece
Austen Bright <pureaustenbright@gmail.com>
2020-03-27 00:31:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,8 +48,8 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(coupon, cart)
     couponed_item_name = "#{coupon} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    binding.pry
     if cart_item && cart_item[:count] >= coupons[i][:num]
-      binding.pry
       if cart_item_with_coupon
         cart_item_with_coupon[i][:count] += coupons[i][:num]
         cart_item[:count] -= coupons[i][:num]
